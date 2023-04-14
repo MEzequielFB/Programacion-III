10cf6d9b5a0de4623a5d68480929295b799181a5
Ezequiel Frisone <ezequiel_frisone@hotmail.com>
2023-04-14 12:39:03 -0300

    Hecho punto 9 del tp 1

--- tp1/p9/Main.java
+++ tp1/p9/Main.java
@@ -2,23 +2,39 @@ public class Main {
     public static void main(String[] args) {
         String s1 = "2421";
         String s2 = "1331";
+        String s3 = "";
+        String s4 = "12321";
         
         System.out.println(esCapicua(s1));
         System.out.println(esCapicua(s2));
-    }
-
-    public static boolean esCapicua2(String string, int indice) {
-        boolean es_capicua = false;
-        if (indice == 341242) {
-
-        }
-        if (indice < 0) {
-
-        }
-        return es_capicua;
+        System.out.println(esCapicua(s3));
+        System.out.println(esCapicua(s4));
     }
 
     public static boolean esCapicua(String string) {
+        String string_invertido = invertir(string, 0);
+        if (string.equals(string_invertido)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static String invertir(String string, int posicion) {
+        if (posicion < 0 || string.length() == 0) {
+            return null;
+        }
+
+        String string_invertido = "";
+        
+        if (posicion >= 0 && posicion < string.length()-1) {
+            string_invertido += invertir(string, posicion+1);
+        }
+
+        string_invertido += string.charAt(posicion);
+        return string_invertido;
+    }
+
+    /* public static boolean esCapicua(String string) {
         String string_invertido = invertir(string);
         for (int i = 0; i < string.length(); i++) {
             if (string.charAt(i) != string_invertido.charAt(i)) {
@@ -34,5 +50,5 @@ public class Main {
             string_invertido += string.charAt(i);
         }
         return string_invertido;
-    }
+    } */
 }
