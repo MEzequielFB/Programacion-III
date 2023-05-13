d7374e9cfcd21f228410c22c56ea660c76331196
Ezequiel Frisone <ezequiel_frisone@hotmail.com>
2023-05-13 13:37:37 -0300

    Agrega punto 3 del tp 3

---
+++ tp3/p3/ServicioDfs.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ServicioDfs {
+    private Grafo<?> grafo;
+
+    public ServicioDfs(Grafo<?> grafo) {
+        this.grafo = grafo;
+    }
+
+    
+
+    public List<Integer> dfs() {
+        ArrayList<Integer> vertices_visitados = new ArrayList<>();
+
+        Iterator<Integer> vertices = grafo.obtenerVertices();
+        while (vertices.hasNext()) {
+            Integer vertice = vertices.next();
+            if (!vertices_visitados.contains(vertice)) {
+                this.dfs_visit(vertice, vertices_visitados);
+            }
+        }
+
+        return vertices_visitados;
+    }
+
+    private void dfs_visit(Integer vertice, ArrayList<Integer> vertices_visitados) {
+        vertices_visitados.add(vertice);
+        Iterator<Integer> adyacentes = this.grafo.obtenerAdyacentes(vertice);
+        while (adyacentes.hasNext()) {
+            Integer adyacente = adyacentes.next();
+            if (!vertices_visitados.contains(adyacente)) {
+                this.dfs_visit(adyacente, vertices_visitados);
+            }
+        }
+    }
+}
