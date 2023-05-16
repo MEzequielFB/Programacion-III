d4ce16ae334cd40789fbc3993d54a28d0ba1a248
Ezequiel Frisone <ezequiel_frisone@hotmail.com>
2023-05-16 19:18:24 -0300

    Modifica metodos, elimina metodo innecesario y comenta codigo del punto 7 del tp 3

--- tp3/p7/ServicioCaminoCorto.java
+++ tp3/p7/ServicioCaminoCorto.java
@@ -16,39 +16,23 @@ public class ServicioCaminoCorto {
     public List<Integer> camino() {
         //Verifica que el grafo tenga ambas esquinas, sino, devuelve null
         if (grafo.contieneVertice(this.esquina_origen) && grafo.contieneVertice(esquina_destino)) {
-            LinkedList<LinkedList<Integer>> caminos = new LinkedList<>();
-            LinkedList<Integer> camino_mas_corto = new LinkedList<>();
-
-            this.obtenerCaminos2(caminos);
-
-            for (LinkedList<Integer> camino : caminos) {
-                if (camino_mas_corto.isEmpty() || camino.size() < camino_mas_corto.size()) {
-                    camino_mas_corto = camino;
-                }
-            }
-            return camino_mas_corto;
+            //Llama al obtenerCaminoMasCorto y retorna el resultado
+            return this.obtenerCaminoMasCorto();
         }
         return null;
     }
 
-    private void obtenerCaminos2(LinkedList<LinkedList<Integer>> caminos) {
+    //Recorrido bfs
+    private LinkedList<Integer> obtenerCaminoMasCorto() {
         LinkedList<Integer> esquinas_visitadas = new LinkedList<>();
         LinkedList<Integer> cola = new LinkedList<>();
 
-        /* boolean esquina_destino_encontrada = false; */
-
+        //El primer elemento aagregado es la esquina origen
         esquinas_visitadas.add(this.esquina_origen);
         cola.add(this.esquina_origen);
 
         while (!cola.isEmpty()) {
             int primer_elemento = cola.removeFirst();
-
-            /* if (esquina_destino_encontrada) {
-                esquinas_visitadas.removeLast();
-                esquina_destino_encontrada = false;
-
-                esquinas_visitadas.add(primer_elemento);
-            } */
 
             int contador = 0;
 
@@ -56,52 +40,26 @@ public class ServicioCaminoCorto {
             while (adyacentes.hasNext()) {
                 contador++;
 
+                //Por cada adyacente del elemento actual se incrementa el contador
+                //Si tiene mas de un adyacente se borra el ultimo elemento de los visitados (adyacente anterior) para interferir en el camino del adyacente actual
                 if (contador > 1) {
                     esquinas_visitadas.removeLast();
                 }
 
+                //Si el adyacente no fue visitado se agrega a la cola y a los visitados
+                //Si el adyacente es la esquina destino se retorna la lista de visitados como el camino mas corto
                 int adyacente = adyacentes.next();
                 if (!esquinas_visitadas.contains(adyacente)) {
-                    /* esquinas_visitadas.add(adyacente); */
+                    esquinas_visitadas.add(adyacente);
                     cola.add(adyacente);
 
-                    LinkedList<Integer> backup_visitadas = new LinkedList<>(esquinas_visitadas);
-                    backup_visitadas.add(adyacente);
-
-                    /* this.agregarCamino(adyacente, caminos, esquinas_visitadas); */
                     if (adyacente == this.esquina_destino) {
-                        caminos.add(new LinkedList<>(backup_visitadas));
-                        /* esquina_destino_encontrada = true; */
+                        return esquinas_visitadas;
                     }
-
-                    esquinas_visitadas = backup_visitadas;
                 }
             }
         }
-    }
-    
-    private void agregarCamino(int esquina, LinkedList<LinkedList<Integer>> caminos, LinkedList<Integer> esquinas_visitadas) {
-        esquinas_visitadas.add(esquina);
-
-        if (esquina == this.esquina_destino) {
-            caminos.add(esquinas_visitadas);
-            return;
-        }
-
-        Iterator<Integer> adyacentes = this.grafo.obtenerAdyacentes(esquina);
-        while (adyacentes.hasNext()) {
-            LinkedList<Integer> backup_visitadas = new LinkedList<>(esquinas_visitadas);
-
-            int adyacente = adyacentes.next();
-            if (!esquinas_visitadas.contains(adyacente)) {
-                if (adyacente == this.esquina_destino) {
-                    esquinas_visitadas.add(adyacente);
-                    caminos.add(esquinas_visitadas);
-                    return;
-                }
-                this.agregarCamino(adyacente, caminos, esquinas_visitadas);
-                esquinas_visitadas = backup_visitadas;
-            }
-        }
+        //Retorna una lista vacia si es que no encuentra una camino de origen a destino
+        return new LinkedList<>();
     }
 }
