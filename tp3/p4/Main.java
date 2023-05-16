8b37120074778043d08e2c3a38170be5754bd81d
Ezequiel Frisone <ezequiel_frisone@hotmail.com>
2023-05-16 18:00:10 -0300

    Modifica el grafo de main y agrega el servicio de camino mas largo

--- tp3/p4/Main.java
+++ tp3/p4/Main.java
@@ -7,31 +7,16 @@ public class Main {
         grafo1.agregarVertice(4);
         grafo1.agregarVertice(5);
         grafo1.agregarVertice(6);
-        grafo1.agregarVertice(7);
 
-        //No borra nada
-        grafo1.borrarVertice(-2);
-        grafo1.borrarVertice(24);
-        //Borra el 2
-        grafo1.borrarVertice(2);
-
-        //No los agrega
-        grafo1.agregarArco(0, 1, "se conecta con");
-        grafo1.agregarArco(1, 0, "se conecta con");
-        //Lo agrega
-        grafo1.agregarArco(1, 4, "se conecta con");
-        grafo1.agregarArco(3, 4, "se conecta con");
         grafo1.agregarArco(1, 3, "se conecta con");
+        grafo1.agregarArco(1, 5, "se conecta con");
         grafo1.agregarArco(1, 6, "se conecta con");
-        grafo1.agregarArco(1, 7, "se conecta con");
-        grafo1.agregarArco(6, 5, "se conecta con");
-        /* grafo1.agregarArco(6, 1, "se conecta con"); */
-        grafo1.agregarArco(5, 1, "se conecta con");
-        grafo1.agregarArco(3, 6, "se conecta con");
-        //Lo borra
-        grafo1.borrarArco(3, 4);
-
-        grafo1.borrarVertice(3);
+        grafo1.agregarArco(2, 1, "se conecta con");
+        grafo1.agregarArco(3, 4, "se conecta con");
+        grafo1.agregarArco(3, 5, "se conecta con");
+        grafo1.agregarArco(4, 5, "se conecta con");
+        grafo1.agregarArco(5, 6, "se conecta con");
+        grafo1.agregarArco(6, 4, "se conecta con");
 
         System.out.println("Cantidad vertices: " + grafo1.cantidadVertices());
         System.out.println("Cantidad arcos: " + grafo1.cantidadArcos());
@@ -39,5 +24,8 @@ public class Main {
         System.out.println("Contiene vertice: " + grafo1.contieneVertice(7));
         System.out.println("Existe arco: " + grafo1.existeArco(3, 4));
         System.out.println("Existe arco: " + grafo1.existeArco(1, 4)+"\n");
+
+        ServicioCaminoLargo servicioCaminoLargo1 = new ServicioCaminoLargo(grafo1, 2, 6);
+        System.out.println("Camino mas largo de 2 a 6: " + servicioCaminoLargo1.camino());
     }
 }
---
+++ tp3/p4/ServicioCaminoLargo.java
@@ -0,0 +1,107 @@
+import java.util.List;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class ServicioCaminoLargo {
+    private Grafo<?> grafo;
+    private int esquina_origen;
+    private int esquina_destino;
+
+    public ServicioCaminoLargo(Grafo<?> grafo, int esquina_origen, int esquina_destino) {
+        this.grafo = grafo;
+        this.esquina_origen = esquina_origen;
+        this.esquina_destino = esquina_destino;
+    }
+
+    public List<Integer> camino() {
+        //Verifica que el grafo tenga ambas esquinas, sino, devuelve null
+        if (grafo.contieneVertice(this.esquina_origen) && grafo.contieneVertice(esquina_destino)) {
+            LinkedList<LinkedList<Integer>> caminos = new LinkedList<>();
+            LinkedList<Integer> camino_mas_largo = new LinkedList<>();
+
+            this.obtenerCaminos2(caminos);
+
+            for (LinkedList<Integer> camino : caminos) {
+                if (camino_mas_largo.isEmpty() || camino.size() > camino_mas_largo.size()) {
+                    camino_mas_largo = camino;
+                }
+            }
+            return camino_mas_largo;
+        }
+        return null;
+    }
+
+    private void obtenerCaminos2(LinkedList<LinkedList<Integer>> caminos) {
+        LinkedList<Integer> esquinas_visitadas = new LinkedList<>();
+        LinkedList<Integer> cola = new LinkedList<>();
+
+        /* boolean esquina_destino_encontrada = false; */
+
+        esquinas_visitadas.add(this.esquina_origen);
+        cola.add(this.esquina_origen);
+
+        while (!cola.isEmpty()) {
+            int primer_elemento = cola.removeFirst();
+
+            /* if (esquina_destino_encontrada) {
+                esquinas_visitadas.removeLast();
+                esquina_destino_encontrada = false;
+
+                esquinas_visitadas.add(primer_elemento);
+            } */
+
+            int contador = 0;
+
+            Iterator<Integer> adyacentes = this.grafo.obtenerAdyacentes(primer_elemento);
+            while (adyacentes.hasNext()) {
+                contador++;
+
+                if (contador > 1) {
+                    esquinas_visitadas.removeLast();
+                }
+
+                int adyacente = adyacentes.next();
+                if (!esquinas_visitadas.contains(adyacente)) {
+                    /* esquinas_visitadas.add(adyacente); */
+                    cola.add(adyacente);
+
+                    LinkedList<Integer> backup_visitadas = new LinkedList<>(esquinas_visitadas);
+                    backup_visitadas.add(adyacente);
+
+                    this.agregarCamino(adyacente, caminos, esquinas_visitadas);
+                    /* if (adyacente == this.esquina_destino) {
+                        caminos.add(new LinkedList<>(backup_visitadas));
+                        //esquina_destino_encontrada = true;
+                    } */
+
+                    esquinas_visitadas = backup_visitadas;
+                }
+            }
+        }
+    }
+    
+    private void agregarCamino(int esquina, LinkedList<LinkedList<Integer>> caminos, LinkedList<Integer> esquinas_visitadas) {
+        esquinas_visitadas.add(esquina);
+
+        if (esquina == this.esquina_destino) {
+            caminos.add(esquinas_visitadas);
+            return;
+        }
+
+        Iterator<Integer> adyacentes = this.grafo.obtenerAdyacentes(esquina);
+        while (adyacentes.hasNext()) {
+            LinkedList<Integer> backup_visitadas = new LinkedList<>(esquinas_visitadas);
+
+            int adyacente = adyacentes.next();
+            if (!esquinas_visitadas.contains(adyacente)) {
+                if (adyacente == this.esquina_destino) {
+                    esquinas_visitadas.add(adyacente);
+                    caminos.add(esquinas_visitadas);
+                    return;
+                }
+                this.agregarCamino(adyacente, caminos, esquinas_visitadas);
+                esquinas_visitadas = backup_visitadas;
+            }
+        }
+    }
+}
