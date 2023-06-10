a06d85d6c6f3f09543a8c6659db328beac3dec9a
Ezequiel Frisone <ezequiel_frisone@hotmail.com>
2023-06-10 20:45:21 -0300

    Hecho punto 7 del tp 5

--- tp5/p7/GreedyJuego.java
+++ tp5/p7/GreedyJuego.java
@@ -17,12 +17,17 @@ public class GreedyJuego {
     public List<Integer> greedy(List<Integer> personajes, int limite_pasos) {
         while (!personajes.isEmpty() && this.hayCazadores(personajes) && this.hayLeones(personajes)) {
             int posicion_cazador = this.seleccionar(personajes, limite_pasos);
+
+            if (posicion_cazador != -1) {
+                personajes.remove(posicion_cazador);
+                this.leones_cazados.add(_leon);
+            }
         }
 
         return this.leones_cazados;
     }
 
-    //VER COMO BORRAR EL LEON SIN AFECTAR LA POSICION DEL CAZADOR
+    //Selecciona la posicion del cazador mas cercano a un leon y elimina al leon de la lista de personajes
     private int seleccionar(List<Integer> personajes, int limite_pasos) {
         int posicion_mejor_candidato = -1;
         int posicion = 0;
@@ -31,7 +36,7 @@ public class GreedyJuego {
         for (int personaje : personajes) {
             if (personaje == _cazador) {
                 int pasos_leon_cercano_actual = this.getPasosLeonCercano(personajes, posicion, limite_pasos);
-                if (pasos_leon_cercano == -1 || ( pasos_leon_cercano_actual != -1 && pasos_leon_cercano_actual < pasos_leon_cercano ) ) {
+                if (pasos_leon_cercano_actual < limite_pasos && pasos_leon_cercano_actual != -1 && ( pasos_leon_cercano_actual < pasos_leon_cercano || pasos_leon_cercano == -1) ) {
                     posicion_mejor_candidato = posicion;
                     pasos_leon_cercano = pasos_leon_cercano_actual;
                 }
@@ -39,9 +44,44 @@ public class GreedyJuego {
 
             posicion++;
         }
+
+        if (posicion_mejor_candidato != -1 && pasos_leon_cercano != -1) {
+            int posicion_borrada = this.borrarLeonCercano(personajes, posicion_mejor_candidato, pasos_leon_cercano);
+            if (posicion_borrada != -1 && posicion_borrada < posicion_mejor_candidato) {
+                posicion_mejor_candidato--;
+            }
+        }
         return posicion_mejor_candidato;
     }
 
+    //Elimina al leon mas cercano a la posicion de un cazador
+    private int borrarLeonCercano(List<Integer> personajes, int posicion_cazador, int cantidad_pasos) {
+        int posicion_leon = -1;
+
+        for (int i = posicion_cazador; i >= 0; i--) {
+            if (personajes.get(i) == _leon) {
+                int pasos_actual = posicion_cazador - i;
+                if (pasos_actual == cantidad_pasos) {
+                    posicion_leon = i;
+                }
+            }
+        }
+
+        if (posicion_leon == -1) {
+            for (int i = posicion_cazador; i < personajes.size(); i++) {
+                if (personajes.get(i) == _leon) {
+                    int pasos_actual = i - posicion_cazador;
+                    if (pasos_actual == cantidad_pasos) {
+                        posicion_leon = i;
+                    }
+                }
+            }
+        }
+        personajes.remove(posicion_leon);
+        return posicion_leon;
+    }
+
+    //Devuelve la cantidad de pasos a la que esta el leon mas cercano a un cazador
     private int getPasosLeonCercano(List<Integer> personajes, int posicion_cazador, int limite_pasos) {
         int pasos = -1;
         for (int i = posicion_cazador; i >= 0; i--) {
--- tp5/p7/Main.java
+++ tp5/p7/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        
+        ArrayList<Integer> personajes = new ArrayList<>();
+        personajes.add(1);
+        personajes.add(0);
+        personajes.add(0);
+        personajes.add(1);
+        personajes.add(0);
+        personajes.add(0);
+        personajes.add(0);
+        personajes.add(1);
+
+        GreedyJuego greedy = new GreedyJuego();
+        System.out.println("Solucion: " + greedy.greedy(personajes, 2));
     }
 }
